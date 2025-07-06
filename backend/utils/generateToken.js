1a9bea7f27777a04b2d69d3104bf16c4c867ac9a
Muhammed Afshan Anas <114726854+afshan5225@users.noreply.github.com>
2025-07-06 16:39:17 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken';
-import dotenv, { configDotenv } from "dotenv"
-configDotenv()
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+export const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '30d',
+  });
 
-export const generateToken =(res,userId)=>{const token = jwt.sign({userId},process.env.JWT_SECRET_KEY,{
-        expiresIn:'30d'
-    })
-    res.cookie('jwt',token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV !=='development',
-        sameSite:'strict',
-        maxAge:30*24*60*60*1000
-    })}
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // ✅ true only on Render
+    sameSite: 'none', // ✅ allow cookie across domains
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  });
+};
