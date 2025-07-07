1e97773cbd6b9f37914c67ba7941769027dace96
Muhammed Afshan Anas <114726854+afshan5225@users.noreply.github.com>
2025-07-07 12:42:55 +0530

    Update constants.js

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL =import.meta.env.VITE_API_BASE_URL;
 export   const PRODUCTS_URL = '/api/products';
 export     const ORDERS_URL = '/api/orders';
 export     const USERS_URL = '/api/users';
